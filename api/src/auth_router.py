6855233eaba61adc0b50f5d2ffadc3420515cef3
obonyojimmy <cliffjimmy27@gmail.com>
2022-07-05 23:52:58 +0300

    implement login & verification endpoints

--- api/src/auth_router.py
+++ api/src/auth_router.py
@@ -21,21 +21,37 @@ async def register():
     return None
 
 @router.post("/login", tags=["Auth"])
-async def login():
+async def login(
+    username: str = Form(..., description="username or email"), #  ie, jimmy@omicmd.com
+    #callback_url: str = Form('http://nucleus.omic.ai:9001', description="the url that will be used in the verify email where verification code will be appended to."), # ie, http://nucleus.omic.ai:9001/
+):
     """
     Initiates a user authetication flow
     """
     ## implemenation links:
     ##   - https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-authentication-flow.html#amazon-cognito-user-pools-server-side-authentication-flow
     ##   - https://github.com/boto/boto3/issues/2745#issuecomment-773021989
-    res, err = cognito.initiate_auth('jimmy@omicmd.com')
-    return None
+    res, err = cognito.initiate_auth(username)
+    print(res, err)
+    username = res['ChallengeParameters']['USERNAME']
+    session = res['Session']
+    resp = {
+        'username': username,
+        'session': session,
+    }
+    return resp
 
-@router.get("/verify", tags=["Auth"])
-async def verify():
+@router.post("/verify", tags=["Auth"])
+async def verify(
+    username: str = Form(..., description="the actual username"), 
+    code: str = Form(..., description="the verification code in the email"), 
+    session: str = Form(..., description="the session response retrieved from login"), 
+):
     """
     Finalizes user authetication flow  by verifying the login-link code sent to user email.
     It returns an Id token on success
     """
+    res, err = cognito.respond_auth_challenge(username, code, session)
+    print(res, err)
     return None
 
--- api/src/services/cognito.py
+++ api/src/services/cognito.py
@@ -21,18 +21,17 @@ def get_secret_hash(username):
     d2 = base64.b64encode(dig).decode()
     return d2
 
-def initiate_auth(username:str):
+def initiate_auth(username:str, callback_url: str = 'http://nucleus.omic.ai:9001'):
     """ 
         Initiate cognito Admin AdminInitiateAuth
 
-        Ref: https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_AdminInitiateAuth.html
+        Ref: https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_AdminInitiateAuth.html 
+        TODO:
+            - find a way to mitigate cognito trigger not passing ClientMetadata to DefineAuthChallenge , 
+              so that we can dynamically pass callbackUrl, 
+              perhaps we can use oauth2.0 Authorization-code flow https://github.com/JinlianWang/aws-lambda-authentication-python/blob/master/app.py
     """
-    client = boto3.client(
-        'cognito-idp',
-        #aws_access_key_id="xxxxxxxxx",
-        #aws_secret_access_key="xxxxxxxxx",
-        #region_name="xxxxxxxxx"
-    )
+    client = boto3.client('cognito-idp', region_name=USER_POOL_REGION)
     secret_hash = get_secret_hash(username)
     try:
         resp = client.admin_initiate_auth(
@@ -44,13 +43,55 @@ def initiate_auth(username:str):
                         'SECRET_HASH': secret_hash,
                     },
                     ClientMetadata = {
-                        'username': username,
+                        'url': callback_url
                     }
+                    #, ContextData = {
+                    #    'IpAddress': '154.159.254.180',
+                    #    'ServerName': callback_url,
+                    #    'ServerPath': '/login',
+                    #    'HttpHeaders': [
+                    #        {
+                    #            'headerName': 'string',
+                    #            'headerValue': 'string'
+                    #        },
+                    #    ],
+                    #}
                 )
     except client.exceptions.NotAuthorizedException:
         return None, "The username or password is incorrect"
     except client.exceptions.UserNotConfirmedException:
         return None, "User is not confirmed"
+    except Exception as e:
+        return None, e.__str__()
+    return resp, None
+
+def respond_auth_challenge(username, code, session):
+    """ 
+        respond to congito custom-auth challenge, uses => AdminRespondToAuthChallenge
+
+        Ref: https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_AdminInitiateAuth.html 
+        TODO:
+            - find a way to mitigate cognito trigger not passing ClientMetadata to DefineAuthChallenge , 
+              so that we can dynamically pass callbackUrl, 
+              perhaps we can use oauth2.0 Authorization-code flow https://github.com/JinlianWang/aws-lambda-authentication-python/blob/master/app.py
+    """
+    client = boto3.client('cognito-idp', region_name=USER_POOL_REGION)
+    challenge_name = 'CUSTOM_CHALLENGE'
+    #username = 'jimmyomicmd.com'
+    #session = 'AYABeB5s1Jyc9ZjuTlJQHjaZbQEAHQABAAdTZXJ2aWNlABBDb2duaXRvVXNlclBvb2xzAAEAB2F3cy1rbXMAS2Fybjphd3M6a21zOnVzLXdlc3QtMjowMTU3MzY3MjcxOTg6a2V5LzI5OTFhNGE5LTM5YTAtNDQ0Mi04MWU4LWRkYjY4NTllMTg2MQC4AQIBAHilsueaVjl36QFS7NU43bur4uuZPopOR6FzLxgbQ6B97gHZiFxvPjBb4GjYeBHhOiL4AAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMjfi0ncYrAZBPVEKNAgEQgDu-PFfwgnScQDEmBPlo1B01L5_mJBcQWYCdS7CkgUR1gT5XvWaDVlLJEkQDzZnGO7AjELcHW8DYa3_SSAIAAAAADAAAEAAAAAAAAAAAAAAAAADx4mA3aji_6iNdF64W5PMl_____wAAAAEAAAAAAAAAAAAAAAEAAAEE_drkpgswvFU5yckNApX-ji3bA2N_RCO1rChvKQvzskk-8a2T-tqFFpVC5qMBIgVJhstAAcv1THUTicCDqOZaH-_C_FuACB7lgI2SXYRpYHe_9sUZXBsWyRwfsf2drW5SigLnWZogil_OfW7he-gmYOmdi7DO3uGDHWM4fCSDzm6pGNXT7dCNx1rZjKLhehfQUSmBg0_QuSEnR6oSBB_SQHtOKKei-2JmsexaO4PS_hVRj4Vu6uIioer6JgkyxSgSPMDocxpQkFP9xmlf2iXvvldZoVM778gWhjXUkGHu6Wjh8uKAMxzRGzm2wLZ2cpPv1YNbFzVemjOvgcpdfL-q5HDuEut4ScGAOMGvw3YD0Zfw794y' 
+    secret_hash = get_secret_hash(username)
+    try:
+        resp = client.admin_respond_to_auth_challenge(
+                    UserPoolId = USER_POOL_ID,
+                    ClientId = CLIENT_ID,
+                    ChallengeName = challenge_name,
+                    ChallengeResponses = {
+                        'USERNAME': username,
+                        'SECRET_HASH': secret_hash,
+                        'ANSWER': code,
+                    },
+                    Session = session
+                )
     except Exception as e:
         return None, e.__str__()
     return resp, None
