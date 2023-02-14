354134395a339f7a34d621215b441e9f037f3e5b
obonyojimmy <cliffjimmy27@gmail.com>
2023-02-14 23:03:29 +0300

    hotfix : user switch workspace / team

--- api/src/user_router.py
+++ api/src/user_router.py
@@ -32,12 +32,12 @@ async def get_user_profile(
     return user_profile
 
 @router.post("/user/workspace", tags=["User"])
-async def add_user_to_workspace_team(
+async def switch_user_workspace_team(
     #user_id: str = Path(..., description="the user-id"),
     x_omic_userid: Union[str, None] = Header(default=None, description="the user-id from header (temporary)"),
     workspace: str = Form(..., description="the workspace name"),
     team: Optional[str] = Form(settings.default_team, description="a team name within the workspace"),
-    membership: str = Form(settings.default_membership_type, description="a the membership of the user in that team. ."),
+    membership: Optional[str] = Form(settings.default_membership_type, description="a the membership of the user in that team. ."),
     db: Session = Depends(deps.get_db),
 ):
     """
@@ -55,16 +55,21 @@ async def add_user_to_workspace_team(
     crud_user_workspace = crud.UserWorkspace(tables.UserWorkspace, db)
     crud_user_team = crud.UserTeam(tables.UserTeam, db)
 
+    # some defaults
+    if not team:
+        team = settings.default_team
+    if not membership:
+        team = settings.default_membership_type
+
     user = crud_utils.get_user(x_omic_userid)
     user_id = user.id
     username = user.username
-
     workspace_obj = crud_workspace.get_by_name(workspace)
     if not workspace_obj:
         raise HTTPException(status_code=400, detail="workspace not exists")
-    
     workspace_id = workspace_obj.id
     workspace_name = workspace_obj.name
+    
     team_obj = crud_team.filter_by(workspace_id=workspace_id, name=team, limit=1)
     team_id = team_obj.id
     if not team_obj:
@@ -81,27 +86,28 @@ async def add_user_to_workspace_team(
         if err:
             raise HTTPException(status_code=400, detail="server error")
     user_workspace = crud_user_workspace.filter_by(user_id, workspace_id, limit=1)
-    if user_workspace:
-        raise HTTPException(status_code=400, detail="user exists in this workspace")
-    db_obj = {
-        'user_id': user_id,
-        'workspace_id': workspace_id,
-        #'team_id': team_id,
-        'membership': settings.default_membership_type, ## by default a user is added as a contributor to a workspace
-    }
-    user_workspace = crud_user_workspace.create(db_obj)
+    if not user_workspace:
+        #raise HTTPException(status_code=400, detail="user exists in this workspace")
+        db_obj = {
+            'user_id': user_id,
+            'workspace_id': workspace_id,
+            #'team_id': team_id,
+            'membership': settings.default_membership_type, ## by default a user is added as a contributor to a workspace
+        }
+        user_workspace = crud_user_workspace.create(db_obj)
     user_team = crud_user_team.filter_by(user_id, workspace_id, team_id=team_id, limit=1)
-    if user_team:
-        raise HTTPException(status_code=400, detail="user exists in this team")
-    team_db_obj = {
-        'user_id': user_id,
-        'workspace_id': workspace_id,
-        'team_id': team_id,
-        'membership': membership,
-    }
-    user_team = crud_user_team.create(team_db_obj)
-    if not user.active_team_id:
-        user = crud_user.set_active_team(user_id, team_id)
+    if not user_team:
+        #raise HTTPException(status_code=400, detail="user exists in this team")
+        team_db_obj = {
+            'user_id': user_id,
+            'workspace_id': workspace_id,
+            'team_id': team_id,
+            'membership': membership,
+        }
+        user_team = crud_user_team.create(team_db_obj)
+    #if not user.active_team_id:
+    # set as active team
+    user = crud_user.set_active_team(user_id, team_id)
     #print(user_workspace)
     return team_db_obj
 
